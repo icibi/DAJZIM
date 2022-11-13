a2ce305d8df41b3b36050afdb71d2efbed66b828
JLaxon <jlaxon@my.centennialcollege.ca>
2022-11-13 01:51:17 -0500

    organized layout and routing and added homepage and navbar

---
+++ server/routes/index.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET templates page. */
+router.get('/templates', function(req, res, next) {
+  res.render('index', { title: 'Templates' });
+});
+
+/* GET pricing page. */
+router.get('/pricing', function(req, res, next) {
+  res.render('index', { title: 'Pricing' });
+});
+
+/* GET previous surveys page. */
+router.get('/previoussurveys', function(req, res, next) {
+  res.render('index', { title: 'PreviousSurveys' });
+});
+
+/* GET create survey page. */
+router.get('/createsurvey', function(req, res, next) {
+  res.render('index', { title: 'CreateSurvey' });
+});
+
+module.exports = router;
